154b7d3c74238648fc9f528112cb0724b69553f6
Thomas Viktil <mandarin@mandarin.no>
2015-05-06 21:59:44 +0200

    Replaced with base url

--- app/middleware/defaultcontent.js
+++ app/middleware/defaultcontent.js
@@ -4,8 +4,8 @@ module.exports = function(page_name) {
     return function(req, res, next) {
         res.content = {
             page: {
-                name:   page_name,
-                url:    config.url('base') + config.routes[page_name]
+                name:       page_name,
+                base_url:   config.url('base')
             }
         };
         next();
